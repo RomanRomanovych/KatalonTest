0a4d621af1a73cb9ae89132ebe470967277ecfa2
User <User@DESKTOP-O4SL0UC>
2024-04-24 01:29:42 +0300

    DataDriven testing Suite

---
+++ Test Suites/DataDrivenLogin.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DataDrivenLogin</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>da20a1e1-0a99-4ac7-878e-f5fca2a986df</testSuiteGuid>
+   <testCaseLink>
+      <guid>c6acb4bb-1a16-450e-a2f1-7d92167330f1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>b0924c25-0f35-4cf8-88d2-1a7a0b9231fb</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/LoginData/LoginData</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>b0924c25-0f35-4cf8-88d2-1a7a0b9231fb</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Login</value>
+         <variableId>34b95092-d022-48fa-970c-044c06ee11a2</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>b0924c25-0f35-4cf8-88d2-1a7a0b9231fb</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>cc5a0ad6-f2f0-42b8-b198-e3d51c7c0020</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
